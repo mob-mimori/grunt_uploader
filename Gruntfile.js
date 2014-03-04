3f12d7da5004a0ae4dfc3a8dbf46efd9320946a3
mimori <mimori@mobcast.jp>
2014-03-04 12:48:19 +0900

    sftp

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,32 @@
 module.exports = function(grunt) {
+
+  var config = grunt.file.readJSON('GruntConfig.json');
   grunt.initConfig({
+
+    sftp:{
+      exec:{
+        files:{'./':'index.html'},
+        options: {
+          // IPアドレス、ログイン情報、サーバー上のパスを指定
+          path: config.sftp_path,
+          host: config.sftp_host,
+          username: config.sftp_user,
+          password: config.sftp_key_password,
+          privateKey: grunt.file.read(config.sftp_key_path),
+          passphrase: config.sftp_key_password,
+          port: config.sftp_port
+        }
+      }
+    },
+
     watch: {
       files: '**/*',
       options: {
         livereload: true
-      }
+      },
+      tasks: [
+        'sftp:exec'
+      ]
     },
 
     connect: {
@@ -12,6 +34,7 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-ssh');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
 
